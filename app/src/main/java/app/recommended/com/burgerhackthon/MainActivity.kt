048bcad763d87b4a5db635c73c0288283eea6dc8
Muneesh <munishmisra007@gmail.com>
2018-10-18 13:09:10 +0530

    initial commit

---
+++ app/src/main/java/app/recommended/com/burgerhackthon/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.recommended.com.burgerhackthon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
